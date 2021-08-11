ad8c8a4caedf69297f941cdba2a407ea9e60c2aa
goserg <gor-serg@bk.ru>
2021-08-11 22:17:25 +0300

    created temporary http handlers for link usecase

--- link/delivery/http/http.go
+++ link/delivery/http/http.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"net/http"
+
 	"github.com/goserg/links/domain"
 	"github.com/labstack/echo/v4"
 )
@@ -12,14 +14,44 @@ type Handler struct {
 func Register(e *echo.Echo, useCase domain.LinkUseCase) {
 	handler := Handler{useCase: useCase}
 
-	e.POST("/create", handler.Get)
-	e.GET("/:h", handler.Create)
+	e.POST("/create", handler.Create)
+	e.GET("/:h", handler.Get)
 }
 
 func (h *Handler) Get(c echo.Context) error {
-	return nil
+	ctx := c.Request().Context()
+
+	hash := c.Param("h")
+
+	link, err := h.useCase.Get(ctx, hash)
+	if err != nil {
+		return c.JSON(http.StatusOK, struct {
+			Url string `json:"url"`
+			Err string `json:"err"`
+		}{
+			Url: "",
+			Err: err.Error(),
+		})
+	}
+	return c.Redirect(http.StatusSeeOther, "http://"+link.URL)
+}
+
+type Link struct {
+	Url string `json:"url"`
 }
 
 func (h *Handler) Create(c echo.Context) error {
-	return nil
+	var link Link
+	if err := c.Bind(&link); err != nil {
+		// todo log
+		return err
+	}
+	ctx := c.Request().Context()
+
+	hash, err := h.useCase.Create(ctx, link.Url)
+	if err != nil {
+		// todo log
+		return err
+	}
+	return c.String(http.StatusOK, hash)
 }
