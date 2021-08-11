8dd565623ad919b468b7b52b565c7fbcc16b4a15
goserg <gor-serg@bk.ru>
2021-08-11 21:33:18 +0300

    implemented logic for inmemory link repo

--- link/repository/inmemory/inmemory.go
+++ link/repository/inmemory/inmemory.go
@@ -2,6 +2,7 @@ package inmemory
 
 import (
 	"context"
+	"errors"
 
 	"github.com/goserg/links/domain"
 )
@@ -17,14 +18,31 @@ func New() domain.LinkRepository {
 	return &r
 }
 
-func (r Repository) Create(ctx context.Context, link domain.Link) error {
-	panic("implement me")
+func (r Repository) Create(_ context.Context, link domain.Link) error {
+	_, exist := r.storage[link.Hash]
+	if exist {
+		return errors.New("link hash is not unique")
+	}
+	r.storage[link.Hash] = link.URL
+	return nil
 }
 
-func (r Repository) Get(ctx context.Context, h string) (*domain.Link, error) {
-	panic("implement me")
+func (r Repository) Get(_ context.Context, h string) (*domain.Link, error) {
+	url, exist := r.storage[h]
+	if !exist {
+		return nil, errors.New("can not find link with hash = " + h)
+	}
+	return &domain.Link{
+		Hash: h,
+		URL:  url,
+	}, nil
 }
 
-func (r Repository) Delete(ctx context.Context, link domain.Link) error {
-	panic("implement me")
+func (r Repository) Delete(_ context.Context, link domain.Link) error {
+	_, exist := r.storage[link.Hash]
+	if exist {
+		return errors.New("can not find link with hash = " + link.Hash)
+	}
+	delete(r.storage, link.Hash)
+	return nil
 }
