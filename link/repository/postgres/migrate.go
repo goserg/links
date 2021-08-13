0dd79b05e7d5b5a9b0b12eb2f0428a5de76dad3b
goserg <gor-serg@bk.ru>
2021-08-14 00:37:54 +0300

    added additional constraints to sql table

--- link/repository/postgres/migrate.go
+++ link/repository/postgres/migrate.go
@@ -5,8 +5,8 @@ import "database/sql"
 func migrate(db *sql.DB) error {
 	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS link (
     id SERIAL PRIMARY KEY,
-    hash TEXT,
-    url TEXT
+    hash TEXT UNIQUE NOT NULL,
+    url TEXT NOT NULL
 )`)
 	return err
 }
